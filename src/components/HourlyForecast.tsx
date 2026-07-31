6e415aac936714a220c39a702d9f49240fe3b4a3
Max Kovalchuk <maximkovalchuk157@gmail.com>
2026-07-31 23:08:12 +0300

    refactor: update semantic html tags in HourlyForecast component and add title "Hourly forecast"

--- src/components/HourlyForecast.tsx
+++ src/components/HourlyForecast.tsx
@@ -7,25 +7,28 @@ interface ForecastProps {
 
 export default function HourlyForecast({ forecast }: ForecastProps) {
   return (
-    <>  
+    <>
       {forecast && (
-        <div className='flex gap-6'>
-          {forecast.list.slice(0, 8).map((item) => (
-            <div key={item.dt} className='flex flex-col items-center'>
-              <p>{item.dt_txt.split(' ')[1].slice(0, 5)}</p>
-              <img
-                src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-                alt={item.weather[0].main}
-                className="w-10 h-10"
-              />
-              <p>{formatTemp(item.main.temp)}</p>
-              <p>{formatTemp(item.main.feels_like)}</p>
-              <p>{item.main.humidity}%</p>
-              <p>{item.main.pressure} hPa</p>
-              <p>{Math.round(item.wind.speed)} m/s</p>
-            </div>
-          ))}
-        </div>
+        <>
+          <h2>Hourly forecast</h2>
+          <ul className='flex gap-6'>
+            {forecast.list.slice(0, 8).map((item) => (
+              <li key={item.dt} className='flex flex-col items-center'>
+                <time dateTime={item.dt_txt}>{item.dt_txt.split(' ')[1].slice(0, 5)}</time>
+                <img
+                  src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                  alt={item.weather[0].main}
+                  className="w-10 h-10"
+                />
+                <p>{formatTemp(item.main.temp)}</p>
+                <p>{formatTemp(item.main.feels_like)}</p>
+                <p>{item.main.humidity}%</p>
+                <p>{item.main.pressure} hPa</p>
+                <p>{Math.round(item.wind.speed)} m/s</p>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
     </>
   )
